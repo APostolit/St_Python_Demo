6bceac247e79b44673df7c376c0f328ebcb684d2
APostolit <50382487+APostolit@users.noreply.github.com>
2025-12-01 14:52:13 +0000

    Add files via upload

---
+++ pages/glava_2/Listing_2_1.py
@@ -0,0 +1,30 @@
+# Листинг 2.1
+a = 100
+print(a)
+
+a = 0.25
+print(a)
+
+a = 'Это первая текстовая строка'
+print(a)
+
+a = 'Это вторая текстовая строка'
+print(a)
+
+a = ['A', 'B', 'C']
+print(a)
+
+a = ('x', 'y', 'z')
+print(a)
+
+a = {'Имя': 'Максим', 'Возраст': 25}
+print(a)
+
+a = {'Имя', False, 45}
+print(a)
+
+a = True
+print(a)
+
+a = None
+print(a)
