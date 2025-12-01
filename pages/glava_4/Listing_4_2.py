394218b5e12a5f29c7584fac1efd7a30dee0a002
APostolit <50382487+APostolit@users.noreply.github.com>
2025-12-01 14:53:27 +0000

    Add files via upload

---
+++ pages/glava_4/Listing_4_2.py
@@ -0,0 +1,18 @@
+# Листинг 4.2
+# Объявление функции
+def my_fun(x,y):
+    c = x + y
+    return c
+
+a = my_fun(1, 2)  # вызов функции
+print('a =', a)
+
+a = 100
+b = 200
+k = my_fun(a, b)  # вызов функции
+print('k =', k)
+
+m = 50
+n = 60
+d = my_fun(m, n)  # вызов функции
+print('d =', d)
